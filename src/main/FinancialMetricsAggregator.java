328eb15b08d37ac66b3ea24425c8c848bf25657d
Arjun Sudheer <arjunsudheer05@outlook.com>
2026-04-20 14:22:47 -0700

    Initialize JavaFX project
    Add BudgetManager and FinancialMetricsAggregator classes

---
+++ src/main/FinancialMetricsAggregator.java
@@ -0,0 +1,17 @@
+package main;
+
+public static class FinancialMetricsAggregator {
+    public static BigDecimal calculateDeposits(Collection<Transaction> transactions) {
+        // TODO: Implement logic to sum all deposit transactions
+        return BigDecimal.ZERO; // Placeholder return value
+    }
+
+    public static BigDecimal calculateSpending(Collection<Transaction> transactions) {
+        // TODO: Implement logic to sum all spending transactions
+        return BigDecimal.ZERO; // Placeholder return value
+    }
+
+    public static BigDecimal calculateNetValue(Collection<Transaction> transactions) {
+        return calculateDeposits(transactions).subtract(calculateSpending(transactions));
+    }
+}
