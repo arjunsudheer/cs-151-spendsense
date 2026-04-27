1b4a727e2b3fc924373826423e31aa7984c3d015
Abdullah Ahmed <121642813+abdullahahmed001@users.noreply.github.com>
2026-04-27 16:42:34 -0700

    Adding Transaction and TransactionSpec.java to main

---
+++ src/main/Transaction.java
@@ -0,0 +1,34 @@
+package main;
+import main.TransactionSpec;
+
+public class Transaction {
+    private static int counter = 0;
+    private final int id;
+    private final TransactionSpec spec;
+
+    public Transaction(TransactionSpec spec) {
+        this.id = generateValidID();
+        this.spec = spec;
+    }
+    public int getID()
+    {
+        return id;
+    }
+
+    public TransactionSpec getSpec()
+    {
+        return new TransactionSpec(
+            spec.getAmount(),
+                spec.getDate(),
+                spec.getDescription(),
+                spec.getProperties()
+       );
+    }
+
+    private int generateValidID()
+    {
+        return counter++;
+    }
+
+}
+
