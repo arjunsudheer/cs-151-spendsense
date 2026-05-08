13374128714a4ea2105923997ef20916722aef72
Abdullah Ahmed <hafahmed.abdullah22@gmail.com>
2026-05-08 12:32:04 -0700

    Readd about page

--- src/main/java/gui/AboutPage.java
+++ src/main/java/gui/AboutPage.java
@@ -1,4 +1,45 @@
 package gui;
 
-public class AboutPage {
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.VBox;
+
+public class AboutPage extends VBox{
+    public AboutPage()
+    {
+        setSpacing(12);
+        setPadding(new Insets(20));
+
+        // Title
+        Label title = new Label("SpendSense Budget Planner");
+        title.setStyle("-fx-font-size: 22px; -fx-font-weight: bold;");
+
+        Label subtitle = new Label("Personal Finance & Budget Tracking System");
+        subtitle.setStyle("-fx-font-size:  14px; -fx-text-fill: #666666;");
+
+        // App Description
+        Label descriptionHeader = new Label("Overview");
+        descriptionHeader.setStyle("-fx-font-size: 16px;  -fx-font-weight: bold;");
+
+        Label description = new Label(
+                 "SpendSense is a JavaFX based application designed to help users track income and expenses, manage spending categories, " +
+                         "and monitor budgets"
+        );
+        description.setWrapText(true);
+
+        // Features
+        Label featuresHeader = new Label("Key Features");
+        featuresHeader.setStyle("-fx-font-size: 16px;  -fx-font-weight: bold;");
+
+        Label features = new Label(
+                "1. Transaction Tracking by Category\n" +
+                        "2. Monthly Budget Management\n" +
+                        "3. Spending Analytics Dashboard\n" +
+                        "4. Global Transaction Search\n" +
+                        "5. Budget Limit Notifications\n" +
+                        "6. Multi-page navigation UI"
+        );
+        features.setWrapText(true);
+    }
 }
